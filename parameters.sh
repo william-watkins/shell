dcf618ef75d9ab55732a4adadaed4f30a3a17439
william-watkins <william@watkins.fr>
2015-04-06 18:52:14 +0200

    .

---
+++ parameters.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "script initialized with $# parameters."
+echo "first parameter is $1"
+# $# display the numbers of parameters entered at the script call
+# $1, $2, ... call the specified parameter in the script 
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# this symbol above is called a "sha-bang", it force the script to run on a specified shell, here bash.
-pwd
-ls

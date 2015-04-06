6baa673330a0342c488932284097d0b142fda42e
william-watkins <william@watkins.fr>
2015-04-06 23:37:03 +0200

    .

--- condition.sh
+++ condition.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-read -p 'pass:' -s pass
-if [ $pass = "test" ]
+echo -e "<< login >>"
+read -p 'pseudo:' -n 25 pseudo		# don't autorise more than 25 characters inputs
+read -p '  pass:' -n 25 -s pass
+
+pseudotemp=`grep -w $pseudo ~/shell/list | cut -d\  -f 1`	# print the line containing the pseudo (if existant) and cut after the space character
+  passtemp=`grep -w $pseudo ~/shell/list | cut -d\  -f 2`	# print the line containing the pseudo (if existant) and cut before the space character
+
+if [[ "$pseudo" = $pseudotemp ]] && [[ "$pass" = $passtemp ]]
 then
-	echo "< access granted >"
-elif [ $pass != "test" ]		# in this simple case, else can simply be used (elif sf: else if)
-then
-	echo "< access denied >"
+	clear
+	echo -e "> access granted."
+else
+	clear
+	echo -e "> access denied."
 fi
 # a condition always begins with if and ends with fi

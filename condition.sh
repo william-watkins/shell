2e42d88add9f7448a85ce6bab5fc858557d343f5
william-watkins <william@watkins.fr>
2015-04-07 01:02:35 +0200

    .

--- condition.sh
+++ condition.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-echo -e "<< login >>"
+red='\e[0;35m'
+blue='\e[0;34m'
+green='\e[0;32m'
+yellow='\e[0;31m'
+neutral='\e[0;m'
+
+clear
+echo -e "< login >${blue}"
 read -p 'pseudo:' -n 25 pseudo		# don't autorise more than 25 characters inputs
 read -p '  pass:' -n 25 -s pass
+time=`date | cut -d , -f 2 | cut -d\  -f 2`
+date=`date | cut -d , -f 1`
 
 pseudotemp=`grep -w $pseudo ~/shell/list | cut -d\  -f 1`	# print the line containing the pseudo (if existant) and cut after the space character
   passtemp=`grep -w $pseudo ~/shell/list | cut -d\  -f 2`	# print the line containing the pseudo (if existant) and cut before the space character
 
 if [[ "$pseudo" = $pseudotemp ]] && [[ "$pass" = $passtemp ]]
 then
-	clear
-	echo -e "> access granted."
+	echo -e "${green}\n> access granted.${neutral}"
+	echo -e "${yellow}> connected: $USER@$DESKTOP_SESSION at $time, $date${neutral}"
 else
-	clear
-	echo -e "> access denied."
+	echo -e "${red}\n> access denied.${neutral}"
 fi
 # a condition always begins with if and ends with fi
